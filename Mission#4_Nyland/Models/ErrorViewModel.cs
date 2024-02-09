60e8d7f9e7be19b1a6a213cdda2af3f4dcba2315
kinseenyland <kinseenyland@gmail.com>
2024-02-08 20:02:24 -0700

    Add project files.

---
+++ Mission#4_Nyland/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission_4_Nyland.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Mission#4_Nyland/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$('#calcbtn').click(function (event) {
+
+    event.preventDefault();
+
+    let hours = parseInt($('#hours').val());
+
+    if (isNaN(hours) || hours <= 0 || hours === null) {
+        alert("Please enter a valid number of hours.");
+        return;
+    }
+
+    let hourlyRate = parseFloat($('#hourly-rate').val().replace('$', ''));
+
+    let total = hours * hourlyRate;
+
+    $('#total').val('$' + total.toFixed(2));
+});
